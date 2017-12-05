c67d135470f102cbf93291b0819872c25364802b
Padmaramani <padma_krithika@hotmail.com>
2017-12-05 00:29:44 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+
+var app = express();
+var PORT = 3000;
+require('./app/routing/htmlRoutes')(app);
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+  
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+})
